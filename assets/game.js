35152596ab5192456a20aa9c4586b31fb8bef18a
Emmanuel6093 <emmanue@Emmanuels-MacBook-Pro.local>
2022-03-07 08:59:31 -0500

    start and end game functions

--- assets/game.js
+++ assets/game.js
@@ -72,8 +72,13 @@ var fight = function(enemyName) {
 }; // end of fight function
 
   
- // fight each enemy-robot by looping over them and fighting them one at a time
-for (var i = 0; i < enemyNames.length; i++) {
+// Function to start a new game
+var startGame = function() {
+  // reset player stats
+  playerHealth = 100;
+  playerAttack = 10;
+  playerMoney = 10; 
+ for (var i = 0; i < enemyNames.length; i++) {
   // if player is still alive, keep fighting
   if (playerHealth > 0) {
     // let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
@@ -92,9 +97,37 @@ for (var i = 0; i < enemyNames.length; i++) {
   else {
     window.alert('You have lost your robot in battle! Game Over!');
     break;
+    }
+  } 
+
+  // function to end the entire game 
+  var endGame = function() {
+    // if player is still alive, player wins!
+  if (playerHealth > 0) {
+    window.alert("Great job, you've survived the game! You now have a score of " + playerMoney + ".");
+  } 
+  else {
+    window.alert("You've lost your robot in battle.");
   }
 }
 
+// ask player if they'd like to play again
+var playAgainConfirm = window.confirm("Would you like to play again?");
+
+if (playAgainConfirm) {
+  // restart the game
+  startGame();
+} 
+else {
+  window.alert("Thank you for playing Robot Gladiators! Come back soon!");
+}
+
+//After the loop ends, player is either out of health or enemies to fight, so run the endGame function
+endGame();
+};
+
+// Start the game when the page loads
+startGame();
 
  
 
